3bcf0ea55fc8373af33a7fce70d45239efd907de
Adam Hilton <adam.tyler.hilton@gmail.com>
2016-12-18 17:13:22 -0600

    User can create a player on the create screen

--- app/src/main/kotlin/com/nonnulldev/underling/ui/create/CreateActivity.kt
+++ app/src/main/kotlin/com/nonnulldev/underling/ui/create/CreateActivity.kt
@@ -1,11 +1,30 @@
 package com.nonnulldev.underling.ui.create
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.KeyListener
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import butterknife.*
 import com.nonnulldev.underling.R
 import com.nonnulldev.underling.ui.base.BaseActivity
+import com.nonnulldev.underling.ui.main.MainActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class CreateActivity : BaseActivity() {
+
+    @BindView(R.id.btnCreate)
+    lateinit var btnCreate: Button
+
+    @BindView(R.id.etPlayerName)
+    lateinit var etPlayerName: EditText
+
 
     @Inject
     lateinit protected var viewModel: CreateScreenViewModel
@@ -14,10 +33,47 @@ class CreateActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
-        initCreateScreenViewModel()
+        ButterKnife.bind(this)
+
+        getActivityComponent().inject(this)
+
+        initBindings()
     }
 
-    private fun initCreateScreenViewModel() {
+    private fun initBindings() {
+        subscriptions.addAll(
+                viewModel.getPlayerNameIsValidObservable()
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe { it -> playerNameIsValid(it) },
 
+                viewModel.getPlayerWasCreatedObservable()
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe { it ->  playerWasCreated(it) }
+        )
+    }
+
+    fun playerNameIsValid(nameIsValid: Boolean) {
+        btnCreate.isEnabled = nameIsValid
+    }
+
+    fun playerWasCreated(created: Boolean) {
+        if(created) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    @OnTextChanged(R.id.etPlayerName)
+    fun onPlayerNameEditTextChanged(name: CharSequence) {
+        viewModel.validatePlayerName(name.toString())
+                .subscribeOn(Schedulers.io())
+                .subscribe()
+    }
+
+    @OnClick(R.id.btnCreate)
+    fun onCreateButtonClicked() {
+        viewModel.createPlayer(etPlayerName.text.toString())
+                .subscribeOn(Schedulers.io())
+                .subscribe()
     }
 }
--- app/src/main/kotlin/com/nonnulldev/underling/ui/create/CreateScreenViewModel.kt
+++ app/src/main/kotlin/com/nonnulldev/underling/ui/create/CreateScreenViewModel.kt
@@ -1,11 +1,48 @@
 package com.nonnulldev.underling.ui.create
 
+import com.nonnulldev.underling.data.local.PlayerRepo
+import com.nonnulldev.underling.data.model.Player
 import com.nonnulldev.underling.injection.scope.PerActivity
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 
 @PerActivity
-class CreateScreenViewModel @Inject constructor() {
+class CreateScreenViewModel @Inject constructor(private val playerRepo: PlayerRepo) {
 
+    private val playerNameIsValidSubject = BehaviorSubject.create<Boolean>()
+    private val playerWasCreatedSubject = BehaviorSubject.create<Boolean>()
 
+    fun validatePlayerName(name: String): Observable<Boolean> {
+        var playerNameIsValid = Observable.just(true)
 
+        if(name.isEmpty() || name.isBlank()) {
+            playerNameIsValid = Observable.just(false)
+        }
+
+        return playerNameIsValid
+                .doOnNext { it -> playerNameIsValidSubject.onNext(it) }
+    }
+
+    fun createPlayer(name: String): Observable<Boolean> {
+
+        var playerCreated = Observable.just(true)
+
+        try {
+            playerRepo.create(Player(name))
+        } catch (e: Exception) {
+            playerCreated = Observable.just(false)
+        }
+
+        return playerCreated
+                .doOnNext { it -> playerWasCreatedSubject.onNext(it) }
+    }
+
+    fun getPlayerNameIsValidObservable(): Observable<Boolean> {
+        return playerNameIsValidSubject
+    }
+
+    fun getPlayerWasCreatedObservable(): Observable<Boolean> {
+        return playerWasCreatedSubject
+    }
 }
